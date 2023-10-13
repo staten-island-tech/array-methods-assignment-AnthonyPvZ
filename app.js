8eb19481715445a0cb08046599dfbac1335c1503
AnthonyPvZ <anthonypvz123@gmail.com>
2023-10-13 09:14:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const people = [
     {firstname:"James", lastname: "Vitolo", age: 17, classes:["Math", "Ela"]},
-    {firstname:"Karen", lastname:"Adams", age: 37, classes:["Ela", "Art"]},
-    {firstname:"Mike", lastname:"Ehrmantraut", age: 60, classes:["Art", "Technology"]},
+    {firstname:"Karen", lastname:"Adams", age: 37, classes:["Math", "Art"]},
+    {firstname:"Mike", lastname:"Ehrmantraut", age: 60, classes:["Ela", "Technology"]},
     {firstname:"Skyler", lastname:"White", age: 39, classes:["Math", "Technology"]}
 ]
-let flatArray = [].concat(...people);
 
 people.forEach((people)=>console.log(people.firstname));
 people.forEach((people)=>console.log(people.lastname));
 people.forEach((people)=>console.log(people.age));
-people.forEach((people)=>console.log(people.classes));
+people.forEach((name) => name.classes.forEach((call) => console.log(call)));
 
 const adults = people.filter(function(person) {
     return person.age > 18
 });
 console.log(adults);
-people.forEach((name) => name.classes.forEach((call) => console.log(call)));
